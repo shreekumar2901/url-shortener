08f1f11cd3f425273b4512b209a3aaff5af3cf85
Shreekumar Hegde <s.shreekumar.c@gmail.com>
2024-12-11 15:40:27 +0100

    Added config
    Signed-off-by: Shreekumar Hegde <s.shreekumar.c@gmail.com>

---
+++ api/config/config.go
@@ -0,0 +1,19 @@
+package config
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+// Gets the value for the key from env
+func Config(key string) string {
+	err := godotenv.Load(".env")
+
+	if err != nil {
+		fmt.Print("Error loading .env file")
+	}
+
+	return os.Getenv(key)
+}
